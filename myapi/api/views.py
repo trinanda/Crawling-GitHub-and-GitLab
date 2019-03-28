59cc718689bdad294ab84b3ae059c7c591069aab
Tri Nanda <zidanecr7kaka@gmail.com>
2019-03-28 08:50:46 +0700

    change flask-restfull to flask-restplus

--- myapi/api/views.py
+++ myapi/api/views.py
@@ -1,12 +1,11 @@
 from flask import Blueprint
-from flask_restful import Api
+from flask_restplus import Api
 
-from myapi.api.resources import UserResource, UserList
+from myapi.api.resources import UserResource
 
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(blueprint)
 
 
-api.add_resource(UserResource, '/users/<int:user_id>')
-api.add_resource(UserList, '/users')
+api.add_resource(UserResource, '/users')
