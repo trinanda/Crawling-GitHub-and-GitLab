6a25452d6c395f727731507ea20de89607aa539c
Tri Nanda <zidanecr7kaka@gmail.com>
2019-03-28 09:53:44 +0700

    change SQLALCHEMY_DATABASE_URI db location

--- myapi/config.py
+++ myapi/config.py
@@ -6,7 +6,11 @@ Use env var to override
 DEBUG = True
 SECRET_KEY = "changeme"
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or  "sqlite:////tmp/myapi.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or  "sqlite:////tmp/myapi.db"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                          'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 JWT_BLACKLIST_ENABLED = True
