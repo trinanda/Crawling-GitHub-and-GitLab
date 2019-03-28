d83c1c2502f63b31d744219a9ed53fbef19f6f47
Tri Nanda <zidanecr7kaka@gmail.com>
2019-03-28 08:48:06 +0700

    adding user model with the same column format with the key from fetch user github data

--- myapi/models/user.py
+++ myapi/models/user.py
@@ -1,19 +1,36 @@
-from myapi.extensions import db, pwd_context
+from myapi.extensions import db
 
 
 class User(db.Model):
-    """Basic user model
-    """
     __tablename__ = "user_v2"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    active = db.Column(db.Boolean, default=True)
-
-    def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
-        self.password = pwd_context.hash(self.password)
-
-    def __repr__(self):
-        return "<User %s>" % self.username
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    login = db.Column(db.String, unique=True, index=True)
+    node_id = db.Column(db.String)
+    avatar_url = db.Column(db.String)
+    gravatar_id = db.Column(db.String)
+    url = db.Column(db.String)
+    html_url = db.Column(db.String)
+    followers_url = db.Column(db.String)
+    following_url = db.Column(db.String)
+    gists_url = db.Column(db.String)
+    starred_url = db.Column(db.String)
+    subscriptions_url = db.Column(db.String)
+    organizations_url = db.Column(db.String)
+    repos_url = db.Column(db.String)
+    events_url = db.Column(db.String)
+    received_events_url = db.Column(db.String)
+    type = db.Column(db.String)
+    site_admin = db.Column(db.Boolean)
+    name = db.Column(db.String)
+    company = db.Column(db.String)
+    blog = db.Column(db.String)
+    location = db.Column(db.String)
+    email = db.Column(db.String)
+    hireable = db.Column(db.String)
+    bio = db.Column(db.String)
+    public_repos = db.Column(db.Integer)
+    public_gists = db.Column(db.Integer)
+    followers = db.Column(db.Integer)
+    following = db.Column(db.Integer)
+    created_at = db.Column(db.String())
+    updated_at = db.Column(db.String())
